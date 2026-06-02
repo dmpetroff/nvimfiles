8fb0f0f1ff3b919db8f93eca639fd90a1ad3dae6
Dmitry Petrov <dmitry.petroff@gmail.com>
2026-06-03 00:03:16 +0300

    markdown tree-sitter queries

---
+++ queries/markdown/locals.scm
@@ -0,0 +1,9 @@
+; All heading in a single query
+(section (atx_heading) @local.definition.section)
+
+; Heading by type
+; (section (atx_heading (atx_h1_marker) heading_content: (_) @local.definition.h1))
+; (section (atx_heading (atx_h2_marker) heading_content: (_) @local.definition.h2))
+; (section (atx_heading (atx_h3_marker) heading_content: (_) @local.definition.h3))
+; (section (atx_heading (atx_h4_marker) heading_content: (_) @local.definition.h4))
+; (section (atx_heading (atx_h5_marker) heading_content: (_) @local.definition.h5))
